3dd5f2a5658bf6b715b876f9c38b547f46c77d85
to <986563206@qq.com>
2021-04-05 17:39:35 +0800

    优化图片显示页面
    自动写入图片张数

--- resourse/img/preview.js
+++ resourse/img/preview.js
@@ -1,3 +1,14 @@
+function Init() {
+    now = 0;
+    var actionimg = document.getElementById("actionimg");
+    for (let i = 0; i < maximg; i++) {
+        createimg(actionimg, "../../temp/" + i + ".jpg")
+    }
+    actionimg.firstChild.style.display = "block";
+    numshow();
+    imgs = actionimg.querySelectorAll("img");
+}
+
 function createimg(node, imgpath) {
     var para = document.createElement("img");
     para.src = imgpath;
@@ -29,25 +40,4 @@ function preimg() {
 
 function numshow() {
     document.querySelector("body > div > div:nth-child(2)").innerHTML = now + 1 + "/" + maximg;
-}
-
-function fileread() {
-    //获取读取我文件的File对象
-    var f = document.getElementById('files').files[0];
-    var name = f.name; //读取选中文件的文件名
-    var size = f.size; //读取选中文件的大小
-    console.log("文件名:" + name + "大小:" + size);
-    var reader = new FileReader(); //这是核心,读取操作就是由它完成.
-    reader.readAsText(f); //读取文件的内容,也可以读取文件的URL
-    reader.onloadend = function () {
-        console.log(this.result);
-        maximg = JSON.parse(this.result).a;
-        Init();
-    }
-}
-
-function inputnum() {
-    maximg = document.getElementById("input").value;
-    console.log(maximg);
-    Init();
 }
---
+++ src/web/ImgCatchApp.java
@@ -0,0 +1,39 @@
+package web;
+
+import java.io.File;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+import web.agent.useragent;
+import web.binary.Img;
+import web.text.Text;
+
+public class ImgCatchApp {
+    public static void main(String[] args) {
+        final String resourse = "resourse\\";// 资源文件夹
+        while (true) {// 循环主程序
+            try {
+                String in = Text.input("请输入正确的网址:");// 获取控制台输入
+                if (in.equals("exit")) {// 输入exit退出主循环
+                    break;
+                }
+                new File("temp").mkdirs();// 新建temp文件夹
+                File html = new File(resourse + "site.html");// 在resource下新建site.html文件
+                URL site = new URL(in);// 将输入转为url
+                HttpURLConnection con = useragent.setagent(site);//
+                Text.save(con.getInputStream(), html);//
+                String str = Text.readtxt(html);//
+                List<String> arr = Text.toImgArray(str);//
+                Img.downloadall(arr, "temp\\");// 下载arr中所有图片url到本地文件夹temp中
+
+                String content = "var maximg ="+arr.size();//
+                Text.writetxt(Img.resourse+"setting.js", content, false);//
+                Img.imgpreview();//
+            } catch (Exception e) {
+                System.out.println("错误：" + e);
+             // e.printStackTrace();
+            }
+        }
+    }
+}
