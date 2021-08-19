63cdb7d66dac7cd94d0efdd5a33ce16a947033df
remu <986563206@qq.com>
2021-08-19 10:44:56 +0800

    rename resource

--- src/main/java/top/tocome/webstore/Data.java
+++ src/main/java/top/tocome/webstore/Data.java
@@ -2,7 +2,7 @@ package top.tocome.webstore;
 
 public class Data {
 
-    public final static String dataPath = "./resourse";
+    public final static String dataPath = "./resource/";
 
-    public final static String cachePath = dataPath + "/temp";
+    public final static String cachePath = dataPath + "/temp/";
 }
